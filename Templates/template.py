1c25ef3ddb3364fc7627997000c848a5d200debb
DoggieLicc <76987398+DoggieLicc@users.noreply.github.com>
2021-02-18 21:58:02 -0500

    remove help command and time module

--- Templates/template.py
+++ Templates/template.py
@@ -1,6 +1,6 @@
 # Change current wordking directory to script "home" directory
 # Important if going to use files or custom modules
-import os, sys, time
+import os, sys
 os.chdir(os.path.dirname(sys.argv[0]))
 
 # Import discord.py and its commands module'
@@ -13,8 +13,9 @@ TOKEN = ""
 # Replace the . to use different prefix for commands
 client = commands.Bot(command_prefix = '.')
 
-# Set the bot's status on discord
-client.change_presence(activity=discord.Game(name="Hello world!"))
+# Set the bot's status on discord, and removes default help command
+client.change_presence(activity=discord.Game(name='Hello world!'))
+client.remove_command('help')
 
 # Event that triggers when bot is ready
 @client.event
