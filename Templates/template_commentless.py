eb7257cd1164c959ae9e477ef57fcacaf413480f
DoggieLicc <76987398+DoggieLicc@users.noreply.github.com>
2021-02-20 15:26:49 -0500

    Update template_commentless.py

--- Templates/template_commentless.py
+++ Templates/template_commentless.py
@@ -1,5 +1,8 @@
 import os, sys
-os.chdir(os.path.dirname(sys.argv[0]))
+try:
+    os.chdir(os.path.dirname(sys.argv[0]))
+except:
+    pass
 
 import discord
 from discord.ext import commands
@@ -7,12 +10,12 @@ from discord.ext import commands
 TOKEN = ""
 
 client = commands.Bot(command_prefix = '.')
-await client.change_presence(activity=discord.Game(name="Hello world!"))
 client.remove_command('help')
 
 @client.event
 async def on_ready():
     print('Ready!')
+    await client.change_presence(activity=discord.Game(name="Hello world!"))
 
 @client.event
 async def on_message(message):
